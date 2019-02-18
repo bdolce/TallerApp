a72c3f557baa9f8b544028fe151a997bdccd05d0
bdolce <37807863+bdolce@users.noreply.github.com>
2019-02-18 17:54:34 -0300

    Refactorización de código en servlets
    .

--- Taller/src/servlet/OrdenesServlet.java
+++ Taller/src/servlet/OrdenesServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class Ordenes
  */
 @WebServlet("/Ordenes")
-public class Ordenes extends HttpServlet {
+public class OrdenesServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public Ordenes() {
+    public OrdenesServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
